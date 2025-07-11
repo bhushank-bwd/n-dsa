71b22d7560c0963e9393a72367ffc45e6520a5a6
bhushan kumbhar <bhushank.bwd@gmail.com>
2025-07-11 20:52:47 +0530

    string 2,3

--- string-1.js
+++ string-1.js
@@ -12,6 +12,42 @@ function lastWordLength(s) {
   }
   return count;
 }
-console.log(lastWordLength("Hello World"));
-console.log(lastWordLength("Hello Master "));
-console.log(lastWordLength(" "));
+function lastWordLength2(s) {
+  let lastIndex = s.length - 1;
+  let count = 0;
+  while (lastIndex >= 0) {
+    if (s[lastIndex] != " ") {
+      ++count;
+    } else if (count > 0) {
+      break;
+    }
+
+    --lastIndex;
+  }
+  return count;
+}
+// console.log(
+//   lastWordLength("Hello World"),
+//   lastWordLength("Hello Master "),
+//   lastWordLength(" ")
+// );
+// console.log(
+//   lastWordLength2("Hello World"),
+//   lastWordLength2("Hello Master "),
+//   lastWordLength2(" ")
+// );
+function findWordContainingChar(wordArr, char) {
+  let res = [];
+  for (let i = 0; i < wordArr.length; i++) {
+    for (let j = 0; j < wordArr[i].length; j++) {
+      if (wordArr[i][j] === char) {
+        res.push(i);
+        break;
+      }
+    }
+  }
+  return res;
+}
+console.log(
+  findWordContainingChar(["abc", "aa", "btu", "sdsda", "sdsffdg"], "a")
+);
